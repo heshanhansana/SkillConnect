1945fe2b25c45c4bf4e10eb238bbf6ff4d606480
K.A.I.Dilum <isiradilumuni@gmail.com>
2025-12-10 12:53:23 +0530

    Cleaned unnecessary files

--- src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-function Card(){
-    return(
-        <div class="bg-white/10 backdrop-blur-xl border border-white/20 p-6 rounded-2xl">
-        <p class="text-white">Glassmorphism with Tailwind</p>
-        </div>
-    );
-}
-export default Card;
